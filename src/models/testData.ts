1edce831655206831caac20e181ee0f537f28def
Rory Dungan <rorydungan@gmail.com>
2018-01-14 14:04:10 +1000

    Added some more test data.

--- src/models/testData.ts
+++ src/models/testData.ts
@@ -1,10 +1,91 @@
-/* tslint:disable */
-
 export const testArtists = {
-	"5a51a895db7f6c1fee00046b": "Post Malone",
-	"5a51a895db7f6c1fee000185": "Ed Sheeran",
-	"5a51a895db7f6c1fee0002e4": "Kendrick Lamar",
-	"5a51a895db7f6c1fee0000b0": "Bruno Mars",
-	"5a51a895db7f6c1fee000020": "Adele",
-	"5a51a895db7f6c1fee00033f": "Lorde",
+    '5a51a895db7f6c1fee00046b': 'Post Malone',
+    '5a51a895db7f6c1fee000185': 'Ed Sheeran',
+    '5a51a895db7f6c1fee0002e4': 'Kendrick Lamar',
+    '5a51a895db7f6c1fee0000b0': 'Bruno Mars',
+    '5a51a895db7f6c1fee`000020': 'Adele',
+    '5a51a895db7f6c1fee00033f': 'Lorde',
+    '5a51a895db7f6c1fee0001dc': 'Future',
 };
+
+/**
+ * Example stats for an artist (in this case, taken from Future)
+ */
+export const testStatsFuture = [
+    {
+        'streams': 3.9458266e-3,
+        'countryCode': 'USA'
+    },
+    {
+        'streams': 2.7318862e-3,
+        'countryCode': 'GBR'
+    },
+    {
+        'streams': 2.4854972e-3,
+        'countryCode': 'AUT'
+    },
+    {
+        'streams': 2.4796436e-3,
+        'countryCode': 'AUS'
+    },
+    {
+        'streams': 3.0908552e-3,
+        'countryCode': 'BEL'
+    },
+    {
+        'streams': 3.7682895e-3,
+        'countryCode': 'CAN'
+    },
+    {
+        'streams': 3.4734672e-3,
+        'countryCode': 'CHE'
+    },
+    {
+        'streams': 2.6963027e-3,
+        'countryCode': 'DEU'
+    },
+    {
+        'streams': 2.6809215e-3,
+        'countryCode': 'DNK'
+    },
+    {
+        'streams': 2.374722e-3,
+        'countryCode': 'FIN'
+    },
+    {
+        'streams': 2.8035508e-3,
+        'countryCode': 'FRA'
+    },
+    {
+        'streams': 6.1578536e-3,
+        'countryCode': 'GRC'
+    },
+    {
+        'streams': 2.6399645e-3,
+        'countryCode': 'HUN'
+    },
+    {
+        'streams': 2.6388548e-3,
+        'countryCode': 'NOR'
+    },
+    {
+        'streams': 2.428746e-3,
+        'countryCode': 'NZL'
+    },
+    {
+        'streams': 3.9716098e-3,
+        'countryCode': 'POL'
+    },
+    {
+        'streams': 2.6323837e-3,
+        'countryCode': 'PRT'
+    },
+    {
+        'streams': 2.6141503e-3,
+        'countryCode': 'SWE'
+    },
+    {
+        'streams': 6.1173225e-3,
+        'countryCode': 'TUR'
+    }
+];
