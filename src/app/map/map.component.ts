a18f6a90e93ebb0f84d2f67d04f71b13f7cb6f64
Rory Dungan <rorydungan@gmail.com>
2018-01-20 14:41:10 +1000

    Worked on map drawing code

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import 'd3-geomap/d3.geomap.min.js';
 import { IArtistInfo } from '../../models/IArtistInfo';
 import { ArtistsService } from '../artists.service';
+import { IArtistStats } from '../../models/IArtistStats';
 
 declare var d3: any;
 declare var colorbrewer: any;
@@ -12,13 +13,27 @@ declare var colorbrewer: any;
     styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-    @Input() artist: IArtistInfo;
+    private map: any;
+
+    @Input('artist')
+    setArtist(value: IArtistInfo) {
+        this.artistsService.getArtistStats(value.id)
+            .then(this.drawMap)
+            .catch(ex => console.error(ex));
+    }
+
+    drawMap(stats: IArtistStats[]) {
+        d3.select('#map')
+            .datum(stats)
+            .call(this.map.draw, this.map);
+    }
 
     constructor(private artistsService: ArtistsService) {
     }
 
     ngOnInit() {
-        const map = d3.geomap.choropleth()
+        // Init map
+        this.map = d3.geomap.choropleth()
             .geofile('/assets/countries.json')
             .colors(colorbrewer.YlGnBu[9])
             .column('streams')
@@ -26,10 +41,7 @@ export class MapComponent implements OnInit {
             .legend(false)
             .unitId('countryCode');
 
-        d3.csv('/assets/test-data.csv', (error, data) => {
-            d3.select('#map')
-                .datum(data)
-                .call(map.draw, map);
-        });
+        // Draw the empty map
+        this.drawMap([]);
     }
 }
