66e456c053d4726dfd61912aaafbabb857586b3b
Rory Dungan <rorydungan@gmail.com>
2018-01-26 14:42:07 +1000

    Delete d3-geomap

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import 'd3-geomap/d3.geomap.min.js';
 import { IArtistInfo } from '../../models/IArtistInfo';
 import { ArtistsService } from '../artists.service';
 import { IArtistStats } from '../../models/IArtistStats';
-
-declare var d3: any;
-declare var colorbrewer: any;
 
 @Component({
     selector: 'app-map',
@@ -32,11 +28,9 @@ export class MapComponent implements OnInit {
         const formattedStats = stats
             .map(v => ({ countryCode: v.countryCode, streams: `${v.streams}` }));
 
-        d3.select('#map').remove();
-
-        d3.select('#map')
-            .datum(formattedStats)
-            .call(this.map.draw, this.map);
+        // d3.select('#map')
+        //     .datum(formattedStats)
+        //     .call(this.map.draw, this.map);
     }
 
     constructor(private artistsService: ArtistsService) {
@@ -44,15 +38,15 @@ export class MapComponent implements OnInit {
 
     ngOnInit() {
         // Init map
-        this.map = d3.geomap.choropleth()
-            .geofile('/assets/countries.json')
-            .colors(colorbrewer.YlGnBu[9])
-            .column('streams')
-            .domain([0, 0.11])
-            .legend(false)
-            .unitId('countryCode');
+        // this.map = d3.geomap.choropleth()
+        //     .geofile('/assets/countries.json')
+        //     .colors(colorbrewer.YlGnBu[9])
+        //     .column('streams')
+        //     .domain([0, 0.11])
+        //     .legend(false)
+        //     .unitId('countryCode');
 
-        // Draw the empty map
-        this.drawMap([]);
+        // // Draw the empty map
+        // this.drawMap([]);
     }
 }
